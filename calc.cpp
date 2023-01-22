9cb9c20c840ca5a54bf5a6b9967806b65c3f6eae
LeksoToriashvili <98639898+LeksoToriashvili@users.noreply.github.com>
2023-01-22 13:27:56 +0400

    Update calc.cpp
    add function romanToArabic

--- calc.cpp
+++ calc.cpp
@@ -2,14 +2,34 @@
 
 using namespace std;
 
+int getInt(char c){
+	switch (c){
+	case 'I': return 1;
+	case 'V': return 5;
+	case 'X': return 10;
+	case 'L': return 50;
+	case 'C': return 100;
+	case 'D': return 500;
+	case 'M': return 1000;
+	}
+	return 0;
+}
+
 bool isNumberValid(string s){
 	bool b;
 	return b;
 }
 
 int romanToArabic(string s){
-	int i;
-	return i;
+	int number=0, i;
+
+	for (i=0; i<s.length(); i++){
+		if (getInt(s[i])<getInt(s[i+1])) number=number-getInt(s[i]);
+	else
+		number=number+getInt(s[i]);
+	}
+
+	return number;
 }
 
 string arabicToRoman(int i){
@@ -24,5 +44,8 @@ bool isOperatorValid(string s){
 
 int main()
 {
+	string rnumber;
+	cin >> rnumber;
+	cout << romanToArabic(rnumber) << endl;
 	return 0;
 }
