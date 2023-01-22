ae1ea95a230e8cc52e464cfbd757e82661f44f08
LeksoToriashvili <98639898+LeksoToriashvili@users.noreply.github.com>
2023-01-22 16:45:02 +0400

    Update calc.cpp
    add function isNumberValid

--- calc.cpp
+++ calc.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int romanToArabic(string s);
+
 int getInt(char c){
 	switch (c){
 	case 'I': return 1;
@@ -16,19 +18,45 @@ int getInt(char c){
 }
 
 bool isNumberValid(string s){
-	bool b;
+	bool b=true;
+	int i, max=1000;
+
+	for (i=0; i<s.length(); i++){
+		if (getInt(s[i])==0) return false;
+		if ((s[i]=='V') || (s[i]=='L') || (s[i]=='D'))
+			if (s[i]==s[i+1]) return false;
+	}
+
+	if (s.length()==1) return true;
+
+	for (i=0; i<s.length()-1; i++){
+		if (getInt(s[i])>max) return false;
+
+		if (getInt(s[i])<getInt(s[i+1])){
+			if (getInt(s[i+1])>max) return false;
+			if ((getInt(s[i+1])/getInt(s[i]))!=5) 
+				if ((getInt(s[i+1])/getInt(s[i]))!=10) return false;
+			max=getInt(s[i]);
+			i++;
+		}
+		else
+		{
+			if (getInt(s[i])>max) return false;
+		}
+	}
+
 	return b;
 }
 
 int romanToArabic(string s){
 	int number=0, i;
 
-	for (i=0; i<s.length(); i++){
+	for (i=0; i<s.length()-1; i++){
 		if (getInt(s[i])<getInt(s[i+1])) number=number-getInt(s[i]);
 	else
 		number=number+getInt(s[i]);
 	}
-
+	number=number+getInt(s[s.length()-1]);
 	return number;
 }
 
@@ -46,6 +74,7 @@ int main()
 {
 	string rnumber;
 	cin >> rnumber;
+	if (isNumberValid(rnumber)==false) cout << "not valid" << endl;
 	cout << romanToArabic(rnumber) << endl;
 	return 0;
 }
